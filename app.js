b7a309bdcf08e4dee8099000cfa54267253e0292
Amos Ho <amosho.tw@yahoo.com.tw>
2017-11-22 13:48:35 +0800

    test

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, {
 		'Context-Type' : 'text/plain'
-	});
+	}); 
+	
 	
 	res.end('test123');
 	console.info('receive request for ' + req.url);
